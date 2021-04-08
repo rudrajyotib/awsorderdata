120f3648ce0092adeec676fd17498d7e33e74909
rudrajyotibiswas <rudrajyoti.biswas@outlook.com>
2021-04-08 09:58:14 +0530

    add bare-bone spring boot project

---
+++ src/main/java/org/learn/aws/orderdata/OrderdataApplication.java
@@ -0,0 +1,13 @@
+package org.learn.aws.orderdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderdataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderdataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/learn/aws/orderdata/OrderdataApplicationTests.java
@@ -0,0 +1,13 @@
+package org.learn.aws.orderdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderdataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
