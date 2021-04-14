82638b2f4e322489179fd82006d2c3f2b3c55f61
rudrajyotibiswas <rudrajyoti.biswas@outlook.com>
2021-04-14 23:00:28 +0530

    test case correction

--- src/test/java/org/learn/aws/orderdata/OrderdataApplicationTests.java
+++ src/test/java/org/learn/aws/orderdata/OrderdataApplicationTests.java
@@ -2,8 +2,10 @@ package org.learn.aws.orderdata;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(locations = "/org/learn/aws/orderdata/persistence/spring-h2-application-context-test.properties")
 class OrderdataApplicationTests {
 
 	@Test
