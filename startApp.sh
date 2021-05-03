1cac5e74ab8fa834a28226b294c00fcbe67c5fcb
rudrajyotibiswas <rudrajyoti.biswas@outlook.com>
2021-05-03 09:20:16 +0530

    start script altered to store pid stop script added.

--- startApp.sh
+++ startApp.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-java -jar -DDB_USER=$DB_USER -DDB_PASS=$DB_PASS -DJDBC_URL=jdbc:postgresql://$DB_HOST:$DB_PORT/$DB_NAME -Dsystem.host.name=$HOSTNAME target/orderdata-0.0.1-SNAPSHOT.jar &
+nohup java -jar -DDB_USER=$DB_USER -DDB_PASS=$DB_PASS -DJDBC_URL=jdbc:postgresql://$DB_HOST:$DB_PORT/$DB_NAME -Dsystem.host.name=$HOSTNAME target/orderdata-0.0.1-SNAPSHOT.jar &
+echo $! > pid.file
